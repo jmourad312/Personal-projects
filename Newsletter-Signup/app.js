8f25989e61baaceaac8d68c6f91fdfe9a429ca71
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-01-02 00:08:49 +0200

    update files in todolist and newsletter projects

---
+++ Newsletter-Signup/app.js
@@ -0,0 +1,77 @@
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+
+const app = express();
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true }));
+
+app.get("/",function (req,res) {
+    res.sendFile(__dirname + "/signup.html")
+})
+
+
+app.post("/",function (req,res) { 
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+    const data = {
+        members:[{
+         email_address: email,
+         status: "subscribed",
+         merge_fields:{
+            FNAME: firstName,
+            LNAME: lastName
+         }   
+        }]
+    }
+    const jsonData = JSON.stringify(data);
+    const url = "https://us7.api.mailchimp.com/3.0/lists/0108d538d3"
+    const options = {
+        method: "POST",
+        auth: process.env.SECRET
+    }
+    const request = https.request(url,options, function (response) {
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html")
+        } else {
+            res.sendFile(__dirname + "/faliure.html")
+        }
+
+
+        response.on("data",function (data) {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end()
+    // console.log(firstName, lastName, email);
+
+})
+
+app.post("/faliure", function (req, res) {
+    res.redirect("/")
+  })
+
+
+app.listen(process.env.PORT || 3000,function () {
+    console.log("welcome to port 3000");
+})
+
+
+
+// {
+//     "email_address": "$user_email",
+//     "status": "subscribed",
+//     "merge_fields": {
+//       "FNAME": "$user_fname",
+//       "LNAME": "$user_lname"
+//     }
+
+// 0108d538d3
