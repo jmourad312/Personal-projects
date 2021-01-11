38c65be14f7795fd3cbf69918e390486903adfd0
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-01-11 09:06:38 +0200

    practice ternary operator and 'AND' operator in react

--- Learning React/my-app/src/components/App.jsx
+++ Learning React/my-app/src/components/App.jsx
@@ -1,18 +1,13 @@
 import React from "react";
-import Login from './Login'
+import Form from "./Form";
 
-var isLoggedIn = false;
-
-const currentTime = new Date(2019, 11, 1, 19).getHours();
-
-console.log(currentTime);
+var userIsRegistered = false;
 
 function App() {
   return (
-    <div className="container">{
-        isLoggedIn ? <h1>Hello</h1> : <Login /> 
-        // currentTime > 12 && <h1>Why are you still working?</h1>
-      }</div>
+    <div className="container">
+    <Form isRegistered={userIsRegistered} />
+    </div>
   );
 }
 
