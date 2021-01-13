c21839d679398f3b0f5c9590ca79d7e865aa23b9
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-01-13 03:29:02 +0200

    Destructuring practice for react

--- Learning React/my-app/src/index.js
+++ Learning React/my-app/src/index.js
@@ -1,22 +1,44 @@
+// CHALLENGE: uncomment the code below and see the car stats rendered
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import cars from './practice';
 
-let time = new Date().toLocaleTimeString();
+const [honda, tesla] = cars;
 
-ReactDOM.render(<App time={time}/>, document.getElementById("root"));
+const {speedStats: {topSpeed:teslaTopSpeed}}= tesla;
+const {speedStats: {topSpeed:hondaTopSpeed}}= honda;
 
-//Challenge:
-//1. Given that you can get the current time using:
-//Show the latest time in the <h1> when the Get Time button
-//is pressed.
+const {coloursByPopularity: [hondaTopColour]} = honda;
+const {coloursByPopularity: [teslaTopColour]} = tesla;
 
-//2. Given that you can get code to be called every second
-//using the setInterval method.
-//Can you get the time in your <h1> to update every second?
 
-//e.g. uncomment the code below to see Hey printed every second.
-// function sayHi() {
-//   console.log("Hey");
-// }
-// setInterval(sayHi, 1000);
+// another solution
+// ----------------
+// const {topSpeed:teslaTopSpeed} = honda.speedStats;
+// const [hondaTopColour] = honda.coloursByPopularity;
+
+// const {topSpeed:hondaTopSpeed} = tesla.speedStats;
+// const [teslaTopColour] = tesla.coloursByPopularity;
+
+
+
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
