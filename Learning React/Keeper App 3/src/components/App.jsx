195254aa2736f4d888700bac7f9affca3f11d27d
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-01-18 08:23:23 +0200

    add keeper app v3

---
+++ Learning React/Keeper App 3/src/components/App.jsx
@@ -0,0 +1,47 @@
+import React,{useState} from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  
+  const [notes, setNotes] = useState([])
+  
+  function addNote (note) {
+    setNotes(previousNotes=>{
+      return [...previousNotes, note];
+    })
+    console.log(note);
+    console.log(notes);
+  }
+
+  function deleteNote (id) {
+    setNotes(previousNotes=>{
+      return previousNotes.filter((noteItem, index)=>{
+        return index !== id;
+      })
+    })
+  }
+  return (
+    <div>
+      <Header />
+      <CreateArea
+      onAdd={addNote}
+      />
+      {notes.map((noteItem,index)=>{
+        return <Note 
+          key={index}
+          id={index}
+          title={noteItem.title}
+          content={noteItem.content}
+          onDelete={deleteNote}
+        />
+      })}
+      {/* <Note key={1} title="Note title" content="Note content" /> */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
