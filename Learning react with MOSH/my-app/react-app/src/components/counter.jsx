a685a5a2b1a326bb7d3089eb7c3c13f3b5b9f063
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-01-25 10:51:46 +0200

    start learning react with mosh

---
+++ Learning react with MOSH/my-app/react-app/src/components/counter.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+export default class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+    //   imgURL:'https://picsum.photos/200'
+  };
+  // styles = {
+  //     fontSize:50,
+  //     fontWeight:'bold'
+  // }
+
+  render() {
+    return (
+      <React.Fragment>
+        {/* <img src={this.state.imgURL} alt=""/> */}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => 
+            <li key={tag}>{tag}</li>
+          )}
+        </ul>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
--- unit testing for react/testing-on-react/src/App.test.js
+++ unit testing for react/testing-on-react/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello Universe/i);
-  expect(linkElement).toBeInTheDocument();
+  // render(<App />);
+  // const linkElement = screen.getByText(/Hello Universe/i);
+  // expect(linkElement).toBeInTheDocument();
+  const div = document.createElement('div');
+  ReactDOM.render(<App/>,div);
+  ReactDOM.unmountComponentAtNode(div);
 });
