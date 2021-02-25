3f40f964cbb70df1fabc5d16e0bee109c9978460
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-02-25 13:32:38 +0200

    new update

--- Dicee Game/index.js
+++ Dicee Game/index.js
@@ -2,8 +2,9 @@ var randomNumber1 = Math.ceil(Math.random()*6);
 var randomNumber2 = Math.ceil(Math.random()*6);
 
 
-document.querySelector(".img1").setAttribute("src","/images/dice"+randomNumber1+".png");
-document.querySelector(".img2").setAttribute("src","/images/dice"+randomNumber2+".png");
+document.querySelector(".img1").setAttribute("src","./images/dice"+randomNumber1+".png");
+
+document.querySelector(".img2").setAttribute("src","./images/dice"+randomNumber2+".png");
 
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "🚩 Player 1 <strong>WINS</strong>"
--- Learning react with MOSH/my-app/react-app/src/components/counter.jsx
+++ Learning react with MOSH/my-app/react-app/src/components/counter.jsx
@@ -3,37 +3,48 @@ import React, { Component } from "react";
 export default class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"],
+    tags: [],
     //   imgURL:'https://picsum.photos/200'
   };
   // styles = {
   //     fontSize:50,
   //     fontWeight:'bold'
   // }
+  renderTages(){
+      if (this.state.tags.length === 0) {
+          return <p>There are no tags!</p>
+      }
+      return (
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      );
+  }
 
   render() {
     return (
       <React.Fragment>
         {/* <img src={this.state.imgURL} alt=""/> */}
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => 
-            <li key={tag}>{tag}</li>
-          )}
-        </ul>
+        {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button> */}
+        <div>
+            {this.state.tags.length === 0 && 'please create a new tag'}
+            {this.renderTages()}
+        </div>
       </React.Fragment>
     );
   }
 
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
+//   getBadgeClasses() {
+//     let classes = "badge m-2 badge-";
+//     classes += this.state.count === 0 ? "warning" : "primary";
+//     return classes;
+//   }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
+//   formatCount() {
+//     const { count } = this.state;
+//     return count === 0 ? "Zero" : count;
+//   }
 }
