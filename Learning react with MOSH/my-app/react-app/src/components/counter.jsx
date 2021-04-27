f956fe59f7add4a05dad7828135b1fd5e0de2ef3
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-04-27 16:38:19 +0200

    update

--- Learning react with MOSH/my-app/react-app/src/components/counter.jsx
+++ Learning react with MOSH/my-app/react-app/src/components/counter.jsx
@@ -1,54 +1,3 @@
-// import React, { Component } from "react";
-
-// export default class Counter extends Component {
-//   state = {
-//     count: 0,
-//     tags: [],
-//     //   imgURL:'https://picsum.photos/200'
-//   };
-//   // styles = {
-//   //     fontSize:50,
-//   //     fontWeight:'bold'
-//   // }
-//   renderTages(){
-//       if (this.state.tags.length === 0) {
-//           return <p>There are no tags!</p>
-//       }
-//       return (
-//         <ul>
-//           {this.state.tags.map((tag) => (
-//             <li key={tag}>{tag}</li>
-//           ))}
-//         </ul>
-//       );
-//   }
-
-//   render() {
-//     return (
-//       <React.Fragment>
-//         {/* <img src={this.state.imgURL} alt=""/> */}
-//         {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-//         <button className="btn btn-secondary btn-sm">Increment</button> */}
-//         <div>
-//             {this.state.tags.length === 0 && 'please create a new tag'}
-//             {this.renderTages()}
-//         </div>
-//       </React.Fragment>
-//     );
-//   }
-
-// //   getBadgeClasses() {
-// //     let classes = "badge m-2 badge-";
-// //     classes += this.state.count === 0 ? "warning" : "primary";
-// //     return classes;
-// //   }
-
-// //   formatCount() {
-// //     const { count } = this.state;
-// //     return count === 0 ? "Zero" : count;
-// //   }
-// }
-
 import React, { Component } from 'react';
 
 class Counter extends Component {
@@ -58,21 +7,58 @@ class Counter extends Component {
       street: "st1",
     },
     imageURL: "https://picsum.photos/200",
+    tags:["t1","t2","t3"]
   };
+
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
 
   // styles={
   //   color:"red",
   //   fontSize: 50,
   // }
+  renderTags(){
+    if(this.state.tags.length ===0) return <p>No Tags</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>  
+    ); 
+  }
+
+  handleIncrement = (id) => {
+    console.log(id);
+    this.setState({
+      count: this.state.count+1
+    })
+  }
   render() {
     return (
       <React.Fragment>
         {/* <img src={this.state.imageURL} alt="" /> */}
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ID:1})}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {/* {this.state.tags.length===0 && "Please create a new tag"} */}
+        {/* {this.renderTags()} */}
       </React.Fragment>
     );
   }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
