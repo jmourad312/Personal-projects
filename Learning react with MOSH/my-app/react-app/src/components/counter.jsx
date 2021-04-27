80ff81aaf934ba7e13eda8351a19a6c98bacea1a
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-04-27 14:25:27 +0200

    setting attributes dynamicly

--- Learning react with MOSH/my-app/react-app/src/components/counter.jsx
+++ Learning react with MOSH/my-app/react-app/src/components/counter.jsx
@@ -52,22 +52,29 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = { 
+  state = {
     count: 0,
     address: {
-      street:"st1",
-    }
+      street: "st1",
+    },
+    imageURL: "https://picsum.photos/200",
   };
-  render() { 
+
+  // styles={
+  //   color:"red",
+  //   fontSize: 50,
+  // }
+  render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        {/* <img src={this.state.imageURL} alt="" /> */}
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
-  formatCount(){
-    const { count } = this.state
+  formatCount() {
+    const { count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
