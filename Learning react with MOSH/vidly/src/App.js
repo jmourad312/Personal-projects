3979ddaea9b9e3030be3200fa4e487a745b7becd
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-05-02 01:31:45 +0200

    working with forms

--- Learning react with MOSH/vidly/src/App.js
+++ Learning react with MOSH/vidly/src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Customers from "./components/customers";
+import LoginForm from "./components/loginForm";
+import MovieForm from "./components/movieForm";
+import Movies from "./components/movies";
+import Navbar from "./components/navbar";
+import NotFound from "./components/notFound";
+import Rentals from "./components/rentals";
 import "./App.css";
-import Movies from './components/movies';
 
 export default class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
-      </main>
+      <>
+        <Navbar />
+        <main className="container">
+          <Switch>
+            <Route path="/login" component={LoginForm} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies"/>
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </>
     );
   }
 }
