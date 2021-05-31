d55709b1d90c057015baae4174b08840a6011747
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-05-31 08:31:01 +0200

    can add, remove and resolve bugs

--- Redux with mosh/redux-starter/src/index.js
+++ Redux with mosh/redux-starter/src/index.js
@@ -1,24 +1,15 @@
 import store from "./store";
-
+import { bugAdded, bugRemoved, bugResolved } from './actions'
 // subscribe method returns a function to unsubscribe in case the user navigates away
-const unsubscribe = store.subscribe(() => {
-  console.log("Store Changed!", store.getState());
-});
+// const unsubscribe = store.subscribe(() => {
+//   console.log("Store Changed!", store.getState());
+// });
+// unsubscribe()
 
-store.dispatch({
-  type: "bugAdded",
-  payload: {
-    description: "Bug1",
-  },
-});
+store.dispatch(bugAdded("Bug 1"));
 
-unsubscribe()
 
-store.dispatch({
-  type: "bugRemoved",
-  payload: {
-    id: 1,
-  },
-});
+// store.dispatch(bugRemoved(1));
+store.dispatch(bugResolved(1));
 
 console.log(store.getState());
