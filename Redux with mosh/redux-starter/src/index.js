6ac09564d7b11e2547a0c1918450c6ca5dfca186
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-05-31 06:55:03 +0200

    immutable.js library

--- Redux with mosh/redux-starter/src/index.js
+++ Redux with mosh/redux-starter/src/index.js
@@ -1,14 +1,10 @@
-const numbers = [1, 2, 3];
+import { Map } from 'immutable';
 
-// adding
-const index = numbers.indexOf(2);
-const added = [...numbers.slice(0, index), 4, ...numbers.slice(index)];
-console.log(added);
+let book = Map({title: "Harry Potter"});
 
-//removing
-const removed = numbers.filter((n) => n !== 2);
-console.log(removed);
+function publish (book) {
+    return book.set("isPublished" ,true);
+}
 
-// updating
-const updated = numbers.map((n) => (n === 2 ? 20 : n));
-console.log(updated);
+book =publish(book);
+console.log(book.toJS());
