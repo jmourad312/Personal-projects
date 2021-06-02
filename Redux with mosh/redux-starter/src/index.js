d589bf71a60b940d36514a028ec8d6fba3936c6b
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-06-02 11:55:54 +0200

    using @redux/toolkit to refactor code

--- Redux with mosh/redux-starter/src/index.js
+++ Redux with mosh/redux-starter/src/index.js
@@ -3,10 +3,14 @@ import * as actions from './store/bugs'
 
 const store = configureStore();
 
-store.dispatch(actions.bugAdded("bug 1"));
-store.dispatch(actions.bugAdded("bug 2"));
-store.dispatch(actions.bugAdded("bug 3"));
-store.dispatch(actions.bugResolved(1));
-// store.dispatch(actions.bugRemoved(2));
+store.subscribe(()=>{
+    console.log("Store Changed!");
+})
+
+store.dispatch(actions.bugAdded({description:"bug 1"}));
+store.dispatch(actions.bugAdded({description:"bug 2"}));
+store.dispatch(actions.bugAdded({description:"bug 3"}));
+store.dispatch(actions.bugResolved({id: 1}));
+store.dispatch(actions.bugRemoved({id:2}));
 console.log(store.getState());
 
