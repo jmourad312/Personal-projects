d2a5817b599b85641ff54aa13bd7cb7c9ba12f1b
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-07-03 23:15:14 +0200

    starting mosters react app

---
+++ Complete React/monsters-rolodex/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      string: "Hello Joe"
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.string}</p>
+          <button>Change Text</button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
