feab56d8cfb3e997954ef3023822058ad0206ff8
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-07-04 23:27:14 +0200

    built basic layout of cards, card list, search bos

--- Complete React/monsters-rolodex/src/App.js
+++ Complete React/monsters-rolodex/src/App.js
@@ -1,30 +1,33 @@
-import React, { Component } from 'react'
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component.jsx";
+import { SearchBox } from "./components/search-box/search-box.component";
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      string: "Hello Joe"
-    }
+      monsters: [],
+      searchField: "",
+    };
   }
-  render(){
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button>Change Text</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <SearchBox
+          placeholder="Search Monsters"
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
