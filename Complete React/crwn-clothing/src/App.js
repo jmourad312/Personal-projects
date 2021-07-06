ea8faac5fec28f54e7736ae80fe697d16b9b98ad
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-07-06 21:37:52 +0200

    update our menu items to navigate withRouter HOC

--- Complete React/crwn-clothing/src/App.js
+++ Complete React/crwn-clothing/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import './App.css';
-import Homepage from './pages/homepage/Hompage.component'
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Homepage from "./pages/homepage/Hompage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
